29b81c283e0bb3b95c44f44b8dfb6178af938f8a
babs12316 <44439609+babs12316@users.noreply.github.com>
2021-05-30 21:46:44 +0200

    onhover pointer will be cursor

--- src/components/Sort/Sort.js
+++ src/components/Sort/Sort.js
@@ -1,14 +1,14 @@
-const Sort = ({sortBy,onSort}) => {
-    const handleAscClick=(e,type)=>{
-        e.preventDefault()
-        onSort(sortBy,type)
-    }
-    return (
-        <>
-        <span onClick={(e)=>handleAscClick(e,'asc')}>Asc &uarr;</span>
-        <span onClick={(e)=>handleAscClick(e,'desc')}>Desc &darr;</span>
-        </>
-      );
-}
- 
+const Sort = ({ sortBy, onSort }) => {
+  const handleAscClick = (e, type) => {
+    e.preventDefault();
+    onSort(sortBy, type);
+  };
+  return (
+    <>
+      <span className="sort" onClick={(e) => handleAscClick(e, "asc")}>&uarr;</span>
+      <span className="sort" onClick={(e) => handleAscClick(e, "desc")}>&darr;</span>
+    </>
+  );
+};
+
 export default Sort;
